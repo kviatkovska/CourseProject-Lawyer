862bc2019153472a722a81431dbcf0f46edf0f13
Anastasiia <43140963+kviatkovska@users.noreply.github.com>
2020-05-01 00:38:34 +0300

    Add files via upload

---
+++ blocks/header.php
@@ -0,0 +1,9 @@
+<header><h1 class="top"><a href="/" title="На головну"><img src="img\header.png">Адвокат<img src="img\header2.png"></a></h1>
+ 	<div class="text">
+<b>Контактна інформація</b> <br>
+<i>Email: advokatrivne@example <br>
+Телефон: 0987654321<br>
+Адрес: вул. Міцкевича, 1<br>
+Графік роботи: Щодня 10:00-18:00</i><br> 
+</div>
+ </header>
