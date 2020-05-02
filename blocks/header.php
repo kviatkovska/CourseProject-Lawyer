b8bb8a94525f87937a822fa82417fc44268f6e2b
Anastasiia <43140963+kviatkovska@users.noreply.github.com>
2020-05-02 23:44:17 +0300

    Update header.php

--- blocks/header.php
+++ blocks/header.php
@@ -1,9 +1,9 @@
-<header><h1 class="top"><a href="/" title="На головну"><img src="img\header.png">Адвокат<img src="img\header2.png"></a></h1>
+<header><h1 class="top"><a href="/" title="На головну"><img src="img\header.png">Адвокат</a></h1>
  	<div class="text">
 <b>Контактна інформація</b> <br>
-<i>Email: advokatrivne@example <br>
-Телефон: 0987654321<br>
-Адрес: вул. Міцкевича, 1<br>
-Графік роботи: Щодня 10:00-18:00</i><br> 
+<i><b>Телефон: 0987654321 <br>
+Адресa: вул. Міцкевича, 1<br>
+Email: advokatrivne@example <br>
+Графік роботи: Щодня 10:00-18:00</i></b><br> 
 </div>
  </header>
