1fb64ad595546b9e89075df8abd6a3a09a6902f9
Anastasiia <43140963+kviatkovska@users.noreply.github.com>
2020-05-02 23:50:11 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -6,11 +6,19 @@
   
  <?php require_once "Z:\home\site\www\blocks\menu.php" ?>
  
-<div class="reg"><b><i>Вхід у систему для адвоката:</b></i><br>
+<div class="reg"><b>Вхід у систему для адвоката:</b><br>
 		<input type="text" name="email" value="" placeholder="Ваш email*"  data-err="Заповніть email" size="40"><br>
 		<input type="text" name="name" value="" placeholder="Ваше ім'я*"  data-err="Заповніть ім'я" size="40"><br>
 		<input type="text" name="pass" value="" placeholder="Пароль*" data-err="Введіть пароль" size="40"><br>
 		<button class="button2">Увійти</button>
 </div>
+
+<footer>
+   <div class="text2" align="center">
+<b>Контактна інформація</b> <br>
+<i><b>Телефон: 0987654321   |   Адресa: вул. Міцкевича, 1   |   Email: advokatrivne@example   |   Графік роботи: Щодня 10:00-18:00</i></b><br> 
+</div>
+ </footer>
+ 
 </body>
 </html>
