cd45121ff06560d0340bfb88a7ef18a2d22ede2e
Anastasiia <43140963+kviatkovska@users.noreply.github.com>
2020-05-02 23:43:37 +0300

    Update head.php

--- blocks/head.php
+++ blocks/head.php
@@ -1,5 +1,5 @@
 <head>
  <meta charset="utf-8">
    <title>Адвокат в м. Рівне</title>
-   <link href="css\style.css" rel="stylesheet" type="text/css" >
+   <link rel="stylesheet" type="text/css" href="css\style.css" >
 </head>
