705b65d9fe6c618e4663e5faf08cf7719fefdb1a
Anastasiia <43140963+kviatkovska@users.noreply.github.com>
2020-05-02 23:48:23 +0300

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -6,14 +6,22 @@
   
  <?php require_once "Z:\home\site\www\blocks\menu.php" ?>
  
-<div class="feedback"><b><i>Заповніть форму, щоб ми зв'язалися з Вами:</b></i><br>
-		<input type="text" name="email" value="" placeholder="Ваш email*"  data-err="Заповніть email" size="40"><br>
-		<input type="text" name="name" value="" placeholder="Ваше ім'я*"  data-err="Заповніть ім'я" size="40"><br>
-		<input type="text" name="tel" value="" placeholder="Ваш телефон*" data-err="Заповніть телефон" size="40"><br>
-		<textarea name="mess" cols="40" rows="10" placeholder="Опишіть Вашу проблему"></textarea>
+<div class="feedback"><b>Заповніть форму, щоб ми зв'язалися з Вами:</b><br>
+		<input type="text" name="email" value="" placeholder="Ваш email*"  data-err="Заповніть email" size="50"><br>
+		<input type="text" name="name" value="" placeholder="Ваше ім'я*"  data-err="Заповніть ім'я" size="50"><br>
+		<input type="text" name="tel" value="" placeholder="Ваш телефон*" data-err="Заповніть телефон" size="50"><br>
+		<textarea name="mess" cols="50" rows="7" placeholder="Опишіть Вашу проблему"></textarea>
 		<div class="ajax-loader"></div>
 		<div id="f1-r2"></div>
 		<button class="button">Відправити</button>
 </div>
+
+<footer>
+   <div class="text2" align="center">
+<b>Контактна інформація</b> <br>
+<i><b>Телефон: 0987654321   |   Адресa: вул. Міцкевича, 1   |   Email: advokatrivne@example   |   Графік роботи: Щодня 10:00-18:00</i></b><br> 
+</div>
+ </footer>
+ 
 </body>
 </html>
