52a6b0617c6bce53161b5cbb3aba09d8619d326b
Anastasiia <43140963+kviatkovska@users.noreply.github.com>
2020-05-14 20:21:09 +0300

    Delete header.php

--- blocks/header.php
+++
@@ -1,9 +0,0 @@
-<header><h1 class="top"><a href="/" title="На головну"><img src="img\header.png">Адвокат</a></h1>
- 	<div class="text">
-<b>Контактна інформація</b> <br>
-<i><b>Телефон: 0987654321 <br>
-Адресa: вул. Міцкевича, 1<br>
-Email: advokatrivne@example <br>
-Графік роботи: Щодня 10:00-18:00</i></b><br> 
-</div>
- </header>
