04b2fba32cc356b84a0fb7a4a3510ce09aa980f3
Anastasiia <43140963+kviatkovska@users.noreply.github.com>
2020-05-17 01:40:49 +0300

    Add files via upload

--- blocks/footer.php
+++ blocks/footer.php
@@ -1,5 +1,6 @@
 <footer>
-<?php
-echo "<p>Copyright &copy; 2020</p>";
-?>
+<ul class="menu">
+<li><a href="http://twitter.com" title="Twitter" target="_blank"><img src="../img/twitter.png"></a></li>
+  <li><a href="http://facebook.com" title="Facebook" target="_blank"><img src="../img/facebook.png"></a></li>
+  </ul>
 </footer>
--- blocks/menu.php
+++ blocks/menu.php
@@ -2,7 +2,20 @@
   <li><a href="index.php">Головна</a></li>
   <li><a href="services.php">Наші послуги</a></li>
   <li><a href="feedback.php">Подати заявку</a></li>
-	 <li><a href="reg.php">Для адвоката<a/></li>  
-  <li><a href="http://twitter.com" title="Twitter" target="_blank"><img src="../img/twitter.png"></a></li>
+  <?php require "login.php" ;
+ if (! empty($_SESSION['success'])) {
+    ?>
+	 <li><a href="../employees.php">Кабінет працівника</a></li>  
+	 <?php
+}
+else
+{
+	?>
+		 <li><a href="../employees.php">Для адвоката</a></li>  
+<?php
+	}
+?>
+    <li><a href="http://twitter.com" title="Twitter" target="_blank"><img src="../img/twitter.png"></a></li>
   <li><a href="http://facebook.com" title="Facebook" target="_blank"><img src="../img/facebook.png"></a></li>
+
 </ul>
